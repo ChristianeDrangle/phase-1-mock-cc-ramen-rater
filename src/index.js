f1fa28a73e259a030c9fc65db1321e1bec6b40ff
Christiane Drangle <positivitee@proton.me>
2023-05-26 17:20:27 -0400

    Shows Ramen Info in Console

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-// write your code here
+// write yourde here
+//add Images
+//connect to api pictures
+getRamen();
+const ramenMenuDiv = document.querySelector("#ramen-menu");
+function getRamen() {
+  fetch("http://localhost:3000/ramens")
+    .then((resp) => resp.json())
+    .then((ramens) => showAllRamen(ramens));
+}
+
+function showAllRamen(ramens) {
+  ramens.forEach((ramen) => renderRamen(ramen));
+}
+
+function renderRamen(ramen) {
+  const ramenImg = document.createElement("img");
+  ramenImg.src = ramen.image;
+  ramenImg.addEventListener("click", () => seeRamenInfo(ramen));
+  ramenMenuDiv.append(ramenImg);
+}
+
+function seeRamenInfo(ramen) {
+  console.log(ramen);
+}
